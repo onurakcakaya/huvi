1a8df43efaf4f143a5a88bf95ee1eab1f40a07d4
onurak10 <huviapp@gmail.com>
2026-01-18 14:15:31 +0300

    logo

--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,7 @@ export default defineConfig({
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: [
-        'favicon.ico',
-        'apple-touch-icon.png',
-        'masked-icon.svg'
-      ],
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'favicon-32x32.png'],
       manifest: {
         name: 'Huvi - Güzellik ve Bakım',
         short_name: 'Huvi',
@@ -25,17 +21,17 @@ export default defineConfig({
         scope: '/',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
